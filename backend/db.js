eae44c3e7f5c7e523e8864e0c40d97910636581f
Juan Pablo Flores <ju.pabloflores@gmail.com>
2026-07-06 00:21:34 -0400

    Preparación para despliegue en Render

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "stgojoven",
-    password: "12345",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,10 @@ app.use(
     "/api/contacto",
     contactoRoutes
 );
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+
 });
